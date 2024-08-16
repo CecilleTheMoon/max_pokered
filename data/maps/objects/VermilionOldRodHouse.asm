104da21c4d4ae561e5b7b487e25640d8da25e252
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-08-16 17:09:21 -0600

    [UPD] Removed FISHING_GURU object_event
    Removed the FISHING_GURU object_event from VermilionOldRodHouse map

--- data/maps/objects/VermilionOldRodHouse.asm
+++ data/maps/objects/VermilionOldRodHouse.asm
@@ -11,6 +11,6 @@ VermilionOldRodHouse_Object:
 	def_bg_events
 
 	def_object_events
-	object_event  2,  4, SPRITE_FISHING_GURU, STAY, RIGHT, TEXT_VERMILIONOLDRODHOUSE_FISHING_GURU
+	;object_event  2,  4, SPRITE_FISHING_GURU, STAY, RIGHT, TEXT_VERMILIONOLDRODHOUSE_FISHING_GURU
 
 	def_warps_to VERMILION_OLD_ROD_HOUSE
