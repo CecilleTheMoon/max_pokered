07383e4ba3624d490a49c3feba699f9ab0e2783b
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-08-17 19:14:35 -0600

    [NEW] Added CERULEAN_CITY warp_event
    Added a new warp event to CERULEAN_CITY map in CERULAN_OLD_ROD_HOUSE map

--- data/maps/objects/CeruleanOldRodHouse.asm
+++ data/maps/objects/CeruleanOldRodHouse.asm
@@ -2,15 +2,16 @@
 	const_export CERULEANOLDRODHOUSE_FISHING_GURU
 
 CeruleanOldRodHouse_Object:
-	db $a ; border block
+	db $c ; border block
 
 	def_warp_events
 	warp_event  2,  7, LAST_MAP, 11
 	warp_event  3,  7, LAST_MAP, 11
+	warp_event  2,  0, LAST_MAP, 12
 
 	def_bg_events
 
 	def_object_events
-	object_event  4,  3, SPRITE_FISHING_GURU, STAY, RIGHT, TEXT_CERULEANOLDRODHOUSE_FISHING_GURU
+	object_event  5,  5, SPRITE_FISHING_GURU, STAY, RIGHT, TEXT_CERULEANOLDRODHOUSE_FISHING_GURU
 
 	def_warps_to CERULEAN_OLD_ROD_HOUSE
