11bcd9911b2eb14eaa9f435276245ede7f3c0317
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-08-20 22:34:57 -0600

    [NEW] Added CeladonGoodRodHouse Text
    Added a new text file for CeladonGoodRodHouse

---
+++ text/CeladonGoodRodHouse.asm
@@ -0,0 +1,44 @@
+_CeladonGoodRodHouseFishingGuruText::
+	text "I'm the FISHING"
+	line "GURU's older"
+	cont "brother!"
+
+	para "I simply Looove"
+	line "fishing!"
+
+	para "Do you like to"
+	line "fish?"
+	done
+
+_CeladonGoodRodHouseFishingGuruReceivedGoodRodText::
+	text "Grand! I like"
+	line "your style!"
+
+	para "Take this and"
+	line "fish, young one!"
+
+	para "<PLAYER> received"
+	line "a @"
+	text_ram wStringBuffer
+	text "!@"
+	text_end
+
+_CeladonGoodRodHouseFishingGuruThatsSoDisappointingText::
+	text "Oh... That's so"
+	line "disappointing..."
+	done
+
+_CeladonGoodRodHouseFishingGuruHowAreTheFishText::
+	text "Hello there,"
+	line "<PLAYER>!"
+
+	para "How are the fish"
+	line "biting?"
+	done
+
+_CeladonGoodRodHouseFishingGuruNoRoomText::
+	text "Oh no!"
+
+	para "You have no room"
+	line "for my gift!"
+	done
