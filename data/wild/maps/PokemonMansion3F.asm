b714b51e2038b0db32902b95c1e3fcbcd778030e
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-20 00:40:12 -0600

    [UPD] Updated PokemonMansion3F.asm
    Updated encounter table file for Pokemon Mansion 3F

--- data/wild/maps/PokemonMansion3F.asm
+++ data/wild/maps/PokemonMansion3F.asm
@@ -1,28 +1,31 @@
 PokemonMansion3FWildMons:
-	def_grass_wildmons 10 ; encounter rate
+	def_grass_wildmons 12 ; encounter rate
+	db 39, RATICATE
 IF DEF(_RED)
-	db 31, KOFFING
-	db 33, GROWLITHE
-	db 35, KOFFING
-	db 32, PONYTA
-	db 34, PONYTA
-	db 40, WEEZING
-	db 34, GRIMER
-	db 38, WEEZING
-	db 36, PONYTA
-	db 42, MUK
+	db 36, KOFFING
+	db 37, GROWLITHE
+	db 37, KOFFING
+	db 37, DITTO
+	db 38, PONYTA
+	db 39, GROWLITHE
+	db 41, WEEZING
+	db 36, GRIMER
+	db 39, PONYTA
+	db 38, MAGMAR
+	db 41, MUK
 ENDC
 IF DEF(_BLUE)
-	db 31, GRIMER
-	db 33, VULPIX
-	db 35, GRIMER
-	db 32, PONYTA
-	db 34, MAGMAR
-	db 40, MUK
-	db 34, KOFFING
-	db 38, MUK
-	db 36, PONYTA
-	db 42, WEEZING
+	db 36, GRIMER
+	db 37, VULPIX
+	db 40, MAGMAR
+	db 37, DITTO
+	db 38, PONYTA
+	db 39, VULPIX
+	db 41, MUK
+	db 36, KOFFING
+	db 39, PONYTA
+	db 40, PONYTA
+	db 41, WEEZING
 ENDC
 	end_grass_wildmons
 
