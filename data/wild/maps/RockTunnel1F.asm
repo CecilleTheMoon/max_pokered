f96b0138484cfde72ebd9300985b9eb0c17cd57c
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 21:01:24 -0600

    [UPD] Updated RockTunnel1F.asm
    Updated encounter table file for Rock Tunnel 1F

--- data/wild/maps/RockTunnel1F.asm
+++ data/wild/maps/RockTunnel1F.asm
@@ -1,15 +1,17 @@
 RockTunnel1FWildMons:
-	def_grass_wildmons 15 ; encounter rate
-	db 16, ZUBAT
+	def_grass_wildmons 12 ; encounter rate
 	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
-	db 15, ZUBAT
-	db 17, MACHOP
-	db 13, ONIX
-	db 15, ONIX
+	db 18, GEODUDE
+	db 19, ZUBAT
+	db 19, MACHOP
+	db 20, GEODUDE
+	db 19, MAGNEMITE
+	db 18, ONIX
+	db 21, MAGNEMITE
+	db 21, MACHOP
+	db 20, ONIX
+	db 21, MACHOP
+	db 22, ONIX
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
