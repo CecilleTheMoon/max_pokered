e4da328ffdcdce25ca600f6b40b612e6adf28ca2
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-20 00:30:54 -0600

    [UPD] Updated SeafoamIslandsB1F.asm
    Updated encounter table file for Seafoam Islands B1F

--- data/wild/maps/SeafoamIslandsB1F.asm
+++ data/wild/maps/SeafoamIslandsB1F.asm
@@ -1,29 +1,17 @@
 SeafoamIslandsB1FWildMons:
-	def_grass_wildmons 10 ; encounter rate
-IF DEF(_RED)
-	db 30, STARYU
-	db 30, HORSEA
-	db 32, SHELLDER
-	db 32, HORSEA
-	db 28, SLOWPOKE
-	db 30, SEEL
-	db 30, SLOWPOKE
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, SEADRA
-ENDC
-IF DEF(_BLUE)
-	db 30, SHELLDER
-	db 30, KRABBY
-	db 32, STARYU
-	db 32, KRABBY
-	db 28, PSYDUCK
-	db 30, SEEL
-	db 30, PSYDUCK
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, KINGLER
-ENDC
+	def_grass_wildmons 12 ; encounter rate
+	db 31, ZUBAT
+	db 33, PSYDUCK
+	db 33, ZUBAT
+	db 33, SLOWPOKE
+	db 33, ZUBAT
+	db 35, GOLBAT
+	db 37, GOLDUCK
+	db 37, SLOWBRO
+	db 34, SLOWPOKE
+	db 37, JYNX
+	db 35, SLOWPOKE
+	db 38, GOLDUCK
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
