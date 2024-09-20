5a3e928a68660d68451de75ae8d41f292c739084
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 21:17:26 -0600

    [UPD] Updated Route12.asm
    Updated encounter table file for route 12

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -1,30 +1,45 @@
 Route12WildMons:
-	def_grass_wildmons 15 ; encounter rate
+	def_grass_wildmons 25 ; encounter rate
+	db 23, PIDGEY
 IF DEF(_RED)
 	db 24, ODDISH
 	db 25, PIDGEY
-	db 23, PIDGEY
+	db 24, ODDISH
+	db 27, PIDGEOTTO
+	db 26, GLOOM
 	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
-	db 27, PIDGEY
+	db 26, TANGELA
+	db 25, VENONAT
 	db 28, GLOOM
-	db 30, GLOOM
+	db 24, BELLSPROUT
+	db 26, VENONAT
 ENDC
 IF DEF(_BLUE)
 	db 24, BELLSPROUT
 	db 25, PIDGEY
-	db 23, PIDGEY
+	db 24, BELLSPROUT
+	db 27, PIDGEOTTO
+	db 26, WEEPINBELL
 	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 27, PIDGEY
+	db 26, TANGELA
+	db 25, VENONAT
 	db 28, WEEPINBELL
-	db 30, WEEPINBELL
+	db 24, ODDISH
+	db 26, VENONAT
 ENDC
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 12 ; encounter rate
+	db 28, GOLDEEN
+	db 28, TENTACOOL
+	db 30, GOLDEEN
+	db 29, TENTACOOL
+	db 28, HORSEA
+	db 29, TENTACOOL
+	db 30, HORSEA
+	db 30, TENTACOOL
+	db 34, SEAKING
+	db 34, TENTACRUEL
+	db 36, SEAKING
+	db 36, TENTACRUEL
 	end_water_wildmons
