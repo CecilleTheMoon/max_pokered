925149e10e8a2e0cc00fae0ab6246b00477627eb
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-20 00:48:23 -0600

    [UPD] Updated CeruleanCaveB1F.asm
    Updated encounter table file for Cerulean Cave B1F

--- data/wild/maps/CeruleanCaveB1F.asm
+++ data/wild/maps/CeruleanCaveB1F.asm
@@ -1,21 +1,30 @@
 CeruleanCaveB1FWildMons:
-	def_grass_wildmons 25 ; encounter rate
-	db 55, RHYDON
-	db 55, MAROWAK
-	db 55, ELECTRODE
-	db 64, CHANSEY
-	db 64, PARASECT
-	db 64, RAICHU
-IF DEF(_RED)
-	db 57, ARBOK
-ENDC
-IF DEF(_BLUE)
-	db 57, SANDSLASH
-ENDC
-	db 65, DITTO
-	db 63, DITTO
-	db 67, DITTO
+	def_grass_wildmons 12 ; encounter rate
+	db 62, GOLBAT
+	db 60, DITTO
+	db 64, GOLBAT
+	db 64, MAROWAK
+	db 65, CHANSEY
+	db 64, MAROWAK
+	db 66, ALAKAZAM
+	db 67, AERODACTYL
+	db 65, GENGAR
+	db 66, RHYDON
+	db 67, GENGAR
+	db 68, RHYDON
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 12 ; encounter rate
+	db 61, GOLBAT
+	db 63, SEADRA
+	db 63, GOLBAT
+	db 64, STARMIE
+	db 65, GYARADOS
+	db 64, CLOYSTER
+	db 65, SEADRA
+	db 67, GYARADOS
+	db 61, DRAGONAIR
+	db 67, LAPRAS
+	db 63, DRAGONAIR
+	db 68, DRAGONITE
 	end_water_wildmons
