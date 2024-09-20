edf3a9971c68fa5a2aa6faa33fdd49b65a434c00
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 20:00:48 -0600

    [NEW] Added PalletTown.asm
    Added encounter table file for Pallet Town

---
+++ data/wild/maps/PalletTown.asm
@@ -0,0 +1,18 @@
+PalletTownWildMons:
+	def_grass_wildmons 0 ; encounter rate
+	end_grass_wildmons
+
+	def_water_wildmons 12 ; encounter rate
+	db 31, TENTACOOL
+	db 31, GOLDEEN
+	db 33, TENTACOOL
+	db 33, GOLDEEN
+	db 33, HORSEA
+	db 33, TENTACOOL
+	db 31, SQUIRTLE
+	db 37, TENTACRUEL
+	db 37, SEADRA
+	db 37, SEAKING
+	db 33, WARTORTLE
+	db 37, SEAKING
+	end_water_wildmons
