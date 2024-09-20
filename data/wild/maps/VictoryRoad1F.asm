4c6362b5e9d9c5c21e7fda3c4c61a54347c0105a
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-20 00:43:40 -0600

    [UPD] Updated VictoryRoad1F.asm
    Updated encounter table file for Victory Road 1F

--- data/wild/maps/VictoryRoad1F.asm
+++ data/wild/maps/VictoryRoad1F.asm
@@ -1,15 +1,17 @@
 VictoryRoad1FWildMons:
-	def_grass_wildmons 15 ; encounter rate
-	db 24, MACHOP
-	db 26, GEODUDE
-	db 22, ZUBAT
-	db 36, ONIX
-	db 39, ONIX
-	db 42, ONIX
-	db 41, GRAVELER
-	db 41, GOLBAT
+	def_grass_wildmons 12 ; encounter rate
+	db 42, GOLBAT
+	db 41, RHYHORN
 	db 42, MACHOKE
-	db 43, MAROWAK
+	db 43, ONIX
+	db 42, GRAVELER
+	db 43, ONIX
+	db 42, MAROWAK
+	db 44, MACHOKE
+	db 44, GRAVELER
+	db 44, MAROWAK
+	db 46, GRAVELER
+	db 46, MAROWAK
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
