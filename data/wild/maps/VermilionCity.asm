505a90a1ec32ce5f8f352ed17a9fa444f04f475e
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 20:52:48 -0600

    [NEW] Added VermilionCity.asm
    Added encounter table file for Vermilion City

---
+++ data/wild/maps/VermilionCity.asm
@@ -0,0 +1,18 @@
+VermilionCityWildMons:
+	def_grass_wildmons 0 ; encounter rate
+	end_grass_wildmons
+
+	def_water_wildmons 12 ; encounter rate
+	db 28, TENTACOOL
+	db 28, GOLDEEN
+	db 30, TENTACOOL
+	db 29, GOLDEEN
+	db 28, HORSEA
+	db 29, GOLDEEN
+	db 30, HORSEA
+	db 30, GOLDEEN
+	db 34, SEAKING
+	db 34, TENTACRUEL
+	db 36, SEAKING
+	db 36, TENTACRUEL
+	end_water_wildmons
