aa4304e8f1d338d141e684326a56e2d7e2dc852c
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 20:40:30 -0600

    [UPD] Added Route3.asm
    Updated encounter table file for route 3

--- data/wild/maps/Route3.asm
+++ data/wild/maps/Route3.asm
@@ -1,15 +1,17 @@
 Route3WildMons:
-	def_grass_wildmons 20 ; encounter rate
+	def_grass_wildmons 25 ; encounter rate
 	db  6, PIDGEY
-	db  5, SPEAROW
-	db  7, PIDGEY
 	db  6, SPEAROW
+	db  7, RATTATA
+	db  7, PIDGEY
+	db  7, RATTATA
 	db  7, SPEAROW
-	db  8, PIDGEY
-	db  8, SPEAROW
-	db  3, JIGGLYPUFF
+	db  8, EEVEE
 	db  5, JIGGLYPUFF
+	db  8, SPEAROW
 	db  7, JIGGLYPUFF
+	db  8, SPEAROW
+	db  9, JIGGLYPUFF
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
