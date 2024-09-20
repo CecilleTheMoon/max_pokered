d597b491f2ba7dd9e452ce7d31d12a9dad1eda0a
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 21:11:24 -0600

    [UPD] Updated PokemonTower4F.asm
    Updated encounter table file for Pokemon Tower 4F

--- data/wild/maps/PokemonTower4F.asm
+++ data/wild/maps/PokemonTower4F.asm
@@ -1,14 +1,16 @@
 PokemonTower4FWildMons:
-	def_grass_wildmons 10 ; encounter rate
-	db 20, GASTLY
+	def_grass_wildmons 12 ; encounter rate
+	db 21, ZUBAT
 	db 21, GASTLY
+	db 22, ZUBAT
 	db 22, GASTLY
-	db 23, GASTLY
-	db 19, GASTLY
-	db 18, GASTLY
-	db 25, HAUNTER
-	db 20, CUBONE
+	db 22, ZUBAT
+	db 22, GASTLY
+	db 21, CUBONE
+	db 23, ZUBAT
 	db 22, CUBONE
+	db 25, HAUNTER
+	db 23, CUBONE
 	db 24, GASTLY
 	end_grass_wildmons
 
