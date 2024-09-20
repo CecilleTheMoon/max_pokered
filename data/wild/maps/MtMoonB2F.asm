c9379b694576b1b688fb213fe57f58e3db060f13
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 20:43:38 -0600

    [UPD] Updated MtMoonB2F.asm
    Updated encounter table file for Mt. Moon B2F

--- data/wild/maps/MtMoonB2F.asm
+++ data/wild/maps/MtMoonB2F.asm
@@ -1,15 +1,17 @@
 MtMoonB2FWildMons:
-	def_grass_wildmons 10 ; encounter rate
+	def_grass_wildmons 12 ; encounter rate
 	db  9, ZUBAT
-	db  9, GEODUDE
-	db 10, ZUBAT
 	db 10, GEODUDE
 	db 11, ZUBAT
 	db 10, PARAS
+	db 11, ZUBAT
+	db 12, GEODUDE
 	db 12, PARAS
+	db 11, ONIX
 	db 10, CLEFAIRY
-	db 12, ZUBAT
+	db 12, ONIX
 	db 12, CLEFAIRY
+	db 13, ONIX
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
