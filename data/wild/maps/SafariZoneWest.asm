02f7567e1e299b24674fd4f6ae101f58f2493138
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-20 00:23:02 -0600

    [UPD] Updated SafariZoneWest.asm
    Updated encounter table file for Safari Zone West

--- data/wild/maps/SafariZoneWest.asm
+++ data/wild/maps/SafariZoneWest.asm
@@ -2,26 +2,40 @@ SafariZoneWestWildMons:
 	def_grass_wildmons 30 ; encounter rate
 IF DEF(_RED)
 	db 25, NIDORAN_M
-	db 26, DODUO
-	db 23, VENONAT
-	db 24, EXEGGCUTE
-	db 33, NIDORINO
-	db 26, EXEGGCUTE
-	db 25, NIDORAN_F
+	db 26, VENONAT
+	db 24, CHARMANDER
+	db 28, NIDORINO
+	db 27, EEVEE
+	db 27, JYNX
+	db 26, NIDORINA
 ENDC
 IF DEF(_BLUE)
 	db 25, NIDORAN_F
-	db 26, DODUO
-	db 23, VENONAT
-	db 24, EXEGGCUTE
-	db 33, NIDORINA
-	db 26, EXEGGCUTE
-	db 25, NIDORAN_M
+	db 26, VENONAT
+	db 24, CHARMANDER
+	db 28, NIDORINA
+	db 27, EEVEE
+	db 27, JYNX
+	db 26, NIDORINO
 ENDC
-	db 31, VENOMOTH
-	db 26, TAUROS
-	db 28, KANGASKHAN
+	db 33, VENOMOTH
+	db 30, MAROWAK
+	db 29, CHANSEY
+	db 32, MAROWAK
+	db 27, TAUROS
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 12 ; encounter rate
+	db 26, GOLDEEN
+	db 26, TENTACOOL
+	db 25, SQUIRTLE
+	db 33, TENTACRUEL
+	db 27, PSYDUCK
+	db 27, SLOWPOKE
+	db 29, GOLDEEN
+	db 29, TENTACOOL
+	db 31, SEAKING
+	db 27, LAPRAS
+	db 33, SEAKING
+	db 29, LAPRAS
 	end_water_wildmons
