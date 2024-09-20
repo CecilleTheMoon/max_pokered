cfde24507ced12be0bc7d140f5c12fd6e70b79a1
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-20 00:11:39 -0600

    [UPD] Updated Route17.asm
    Updated encounter table file for route 17

--- data/wild/maps/Route17.asm
+++ data/wild/maps/Route17.asm
@@ -1,16 +1,30 @@
 Route17WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 25, RATICATE
-	db 24, DODUO
+	db 23, SPEAROW
+	db 23, DODUO
+	db 25, SPEAROW
 	db 27, RATICATE
-	db 26, DODUO
-	db 28, DODUO
-	db 29, RATICATE
-	db 25, FEAROW
+	db 24, PONYTA
+	db 27, RATICATE
+	db 26, FARFETCHD
+	db 26, PONYTA
+	db 25, DODUO
 	db 27, FEAROW
+	db 25, DODUO
+	db 29, FEAROW
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 12 ; encounter rate
+	db 28, TENTACOOL
+	db 28, GOLDEEN
+	db 29, TENTACOOL
+	db 30, GOLDEEN
+	db 28, HORSEA
+	db 29, TENTACOOL
+	db 30, HORSEA
+	db 30, TENTACOOL
+	db 35, SEAKING
+	db 34, TENTACRUEL
+	db 35, SEADRA
+	db 36, TENTACRUEL
 	end_water_wildmons
