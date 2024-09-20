faff6e366a80d7cc03c736b8fc3d91be85d62dcc
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-19 20:21:42 -0600

    [UPD] Updated Route2.asm
    Updated encounter table file for route 2

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -2,22 +2,27 @@ Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db  3, RATTATA
 	db  3, PIDGEY
-	db  4, PIDGEY
 	db  4, RATTATA
-	db  5, PIDGEY
+	db  4, PIDGEY
 IF DEF(_RED)
 	db  3, WEEDLE
-	db  2, RATTATA
-	db  5, RATTATA
+	db  4, NIDORAN_M
+	db  5, PIDGEY
+	db  6, NIDORAN_M
 	db  4, WEEDLE
-	db  5, WEEDLE
+	db  5, RATTATA
+	db  3, CATERPIE
+	db  5, RATTATA
 ENDC
 IF DEF(_BLUE)
 	db  3, CATERPIE
-	db  2, RATTATA
-	db  5, RATTATA
+	db  4, NIDORAN_F
+	db  5, PIDGEY
+	db  6, NIDORAN_F
 	db  4, CATERPIE
-	db  5, CATERPIE
+	db  5, RATTATA
+	db  3, WEEDLE
+	db  5, RATTATA
 ENDC
 	end_grass_wildmons
 
