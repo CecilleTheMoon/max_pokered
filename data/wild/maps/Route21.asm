9e6f8a42f8a5ebb444dfbbd28145059541e49909
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-20 00:28:01 -0600

    [UPD] Updated Route21.asm
    Updated encounter table file for route 21

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -1,26 +1,30 @@
 Route21WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 21, RATTATA
-	db 23, PIDGEY
-	db 30, RATICATE
-	db 23, RATTATA
-	db 21, PIDGEY
-	db 30, PIDGEOTTO
+	db 28, RATTATA
+	db 28, PIDGEY
+	db 30, RATTATA
+	db 30, PIDGEY
+	db 32, RATICATE
 	db 32, PIDGEOTTO
-	db 28, TANGELA
-	db 30, TANGELA
-	db 32, TANGELA
+	db 31, BULBASAUR
+	db 31, CHARMANDER
+	db 34, RATICATE
+	db 34, PIDGEOTTO
+	db 33, IVYSAUR
+	db 33, CHARMELEON
 	end_grass_wildmons
 
-	def_water_wildmons 5 ; encounter rate
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACOOL
-	db 40, TENTACOOL
+	def_water_wildmons 12 ; encounter rate
+	db 31, TENTACOOL
+	db 31, GOLDEEN
+	db 33, TENTACOOL
+	db 33, GOLDEEN
+	db 33, HORSEA
+	db 33, TENTACOOL
+	db 31, SQUIRTLE
+	db 37, TENTACRUEL
+	db 37, SEADRA
+	db 37, SEAKING
+	db 33, WARTORTLE
+	db 37, SEAKING
 	end_water_wildmons
