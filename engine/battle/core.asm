d9e2bb1196134279775aab46a12f0e2319857659
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-26 00:28:04 -0600

    [FIX] Fixed 1/256 miss glitch
    Fixed a bug where moves with 100% accuracy could still miss 1/256 of the time

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5469,6 +5469,12 @@ MoveHitTest:
 .doAccuracyCheck
 ; if the random number generated is greater than or equal to the scaled accuracy, the move misses
 ; note that this means that even the highest accuracy is still just a 255/256 chance, not 100%
+
+	; The following snippet is taken from Pokemon Crystal, it fixes the above bug
+	ld a, b
+	cp $FF ; is the value FF?
+	ret z ; if so, we need not calculate, just so we can fix this bug.
+
 	call BattleRandom
 	cp b
 	jr nc, .moveMissed
