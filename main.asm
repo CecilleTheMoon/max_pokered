aff147a74aaac5d9b6705be5a4d39cae5ab1d400
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-09-30 19:04:39 -0600

    [FIX] Fixed CUT overworld bug
    Fixed a bug where CUT's overworld animation would crash the game

--- main.asm
+++ main.asm
@@ -345,6 +345,11 @@ INCLUDE "engine/menus/league_pc.asm"
 INCLUDE "engine/events/hidden_items.asm"
 
 
+SECTION "bank1D", ROMX
+
+INCLUDE "engine/overworld/cut2.asm"
+INCLUDE "engine/overworld/dust_smoke.asm"
+
 SECTION "bank1E", ROMX
 
 INCLUDE "engine/battle/animations.asm"
@@ -352,10 +357,9 @@ INCLUDE "data/moves/animations.asm"
 INCLUDE "data/battle_anims/subanimations.asm"
 INCLUDE "data/battle_anims/frame_blocks.asm"
 
+
 SECTION "bank1F" , ROMX
 
-INCLUDE "engine/overworld/cut2.asm"
-INCLUDE "engine/overworld/dust_smoke.asm"
 INCLUDE "gfx/fishing.asm"
 INCLUDE "engine/movie/evolution.asm"
 INCLUDE "engine/overworld/elevator.asm"
