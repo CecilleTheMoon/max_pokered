105c2f8363a90fee383b90c0d56c5a754ea46a5d
Tabatha Cecille <142866680+CecilleTheMoon@users.noreply.github.com>
2024-10-03 23:26:09 -0600

    [UPD] Updated ParalyzeEffect
    Updated ParalyzeEffect to prevent duplicate actions. PARALYZE_EFFECT will always succeed on a damaging move if it hits, and will only perform its animation once.

--- engine/battle/move_effects/paralyze.asm
+++ engine/battle/move_effects/paralyze.asm
@@ -26,7 +26,16 @@ ParalyzeEffect_:
 	jr z, .doesntAffect
 .hitTest
 	push hl
+	ldh a, [hWhoseTurn]
+	and a
+	ld a, [wPlayerMovePower]
+	jr z, .gotUsersPower1
+	ld a, [wEnemyMovePower]
+.gotUsersPower1
+	and a
+	jr nz, .skipMoveHitTest
 	callfar MoveHitTest
+.skipMoveHitTest
 	pop hl
 	ld a, [wMoveMissed]
 	and a
@@ -35,7 +44,16 @@ ParalyzeEffect_:
 	callfar QuarterSpeedDueToParalysis
 	ld c, 30
 	call DelayFrames
+	ldh a, [hWhoseTurn]
+	and a
+	ld a, [wPlayerMovePower]
+	jr z, .gotUsersPower
+	ld a, [wEnemyMovePower]
+.gotUsersPower
+	and a
+	jr nz, .skipAnimation
 	callfar PlayCurrentMoveAnimation
+.skipAnimation
 	jpfar PrintMayNotAttackText
 .didntAffect
 	ld c, 50
